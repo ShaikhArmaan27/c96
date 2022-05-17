40c4ac4e875d2798c1f1655fdc05b5825c722d06
Shaikh Armaan <skmdarmaan27@gmail.com>
2022-05-17 22:29:14 +0530

    story

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBi_M1wQUh-mLKPNGFljT5lebIPVncdZN4",
+    authDomain: "story-telling-6d2d5.firebaseapp.com",
+    projectId: "story-telling-6d2d5",
+    storageBucket: "story-telling-6d2d5.appspot.com",
+    messagingSenderId: "950899077823",
+    appId: "1:950899077823:web:c9d7ac11b0ca44de2e0c0a"
+  };
+  export default firebaseConfig
